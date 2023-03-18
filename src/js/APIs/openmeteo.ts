f5e418587765a23ec10ea3febf1eaecde6d2cbf6
Qusz <de.gorchakov@gmail.com>
2023-03-18 11:43:31 +0300

    adds API module

---
+++ src/js/APIs/openmeteo.ts
@@ -0,0 +1,28 @@
+export default async function (lat: number, lng: number): Promise<any> {
+  const url: URL = new URL('https://api.open-meteo.com/v1/forecast');
+  url.searchParams.set('latitude', lat.toString());
+  url.searchParams.set('longitude', lng.toString());
+  url.searchParams.set(
+    'hourly',
+    'temperature_2m,relativehumidity_2m,apparent_temperature,weathercode,pressure_msl,windspeed_10m'
+  );
+  url.searchParams.set(
+    'daily',
+    'weathercode,sunrise,sunset&current_weather=true'
+  );
+  url.searchParams.set('timezone', 'auto');
+
+  try {
+    const request = await fetch(url);
+
+    const response = await request.json();
+
+    if (response.error) {
+      throw new Error('Can\t load the weather at the moment. Try again later');
+    }
+
+    return response;
+  } catch {
+    throw new Error('Something went wrong... Please try again later.');
+  }
+}
