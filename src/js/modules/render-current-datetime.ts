eb3b8b05eb9b3a5fa146abc485a5a4f2599eae93
Qusz <de.gorchakov@gmail.com>
2023-03-19 13:18:15 +0300

    adds mobule

---
+++ src/js/modules/render-current-datetime.ts
@@ -0,0 +1,9 @@
+import getDateTime from '../utils/datetime';
+
+export default function (time: string, timezone: string): void {
+  const element: HTMLParagraphElement | null = document.querySelector(
+    '.weather-card__date'
+  );
+
+  element.textContent = getDateTime(time, timezone, 'long');
+}
