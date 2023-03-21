dc2e65fcab002020239a040b5344b8ba4f4bed31
Qusz <de.gorchakov@gmail.com>
2023-03-21 19:44:28 +0300

    fixes incorrect args order

--- src/js/modules/render-current-datetime.ts
+++ src/js/modules/render-current-datetime.ts
@@ -1,9 +1,9 @@
-import getDateTime from '../utils/datetime';
+import getDateTime from 'utils/datetime';
 
 export default function (time: string, timezone: string): void {
   const element: HTMLParagraphElement | null = document.querySelector(
     '.weather-card__date'
   );
 
-  element.textContent = getDateTime(time, timezone, 'long');
+  element!.textContent = getDateTime('long', time, timezone);
 }
