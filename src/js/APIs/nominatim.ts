721357a84cdac3904d4c4eaeaa78b83272df4511
Qusz <de.gorchakov@gmail.com>
2023-03-22 14:57:01 +0300

    adds better typing

--- src/js/APIs/nominatim.ts
+++ src/js/APIs/nominatim.ts
@@ -1,6 +1,8 @@
 import { NominatimResponse } from 'types/types';
 
-export default async function (query: string): Promise<NominatimResponse[]> {
+export default async function (
+  query: string
+): Promise<NominatimResponse[] | null> {
   const url: URL = new URL('https://nominatim.openstreetmap.org/search');
   url.searchParams.set('city', query);
   url.searchParams.set('format', 'json');
@@ -8,15 +10,19 @@ export default async function (query: string): Promise<NominatimResponse[]> {
   url.searchParams.set('accept-languag', 'en-US');
 
   try {
-    const request = await fetch(url);
-    const response = await request.json();
+    const request: Response = await fetch(url);
+    const response: NominatimResponse[] = await request.json();
 
     if (!response.length) {
       throw new Error("Can't find this location. Please try again.");
     }
 
     return response;
-  } catch (error: any) {
-    throw new Error(error.message);
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      throw new Error(error.message);
+    }
+
+    return null;
   }
 }
