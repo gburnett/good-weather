9a95b47598737cf9dda295411d1379b368b8c87e
Qusz <de.gorchakov@gmail.com>
2023-03-22 14:14:51 +0300

    imports and enables modules

--- src/js/main.ts
+++ src/js/main.ts
@@ -1,8 +1,12 @@
 import 'normalize.css';
 import '@/scss/style.scss';
 
-import showForecast from './modules/show-forecast';
+import showForecast from './modules/toggle-forecast';
+import search from './modules/location-search';
+import loadContent from './modules/load-content';
 
 (() => {
   showForecast();
+  loadContent();
+  search();
 })();
