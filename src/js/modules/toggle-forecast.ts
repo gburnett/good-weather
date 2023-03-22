232adafef7e0484d8303efc60552d1fca61f40cc
Qusz <de.gorchakov@gmail.com>
2023-03-22 19:24:44 +0300

    now toggle happens when clicked on wrapper, not the entire header

--- src/js/modules/toggle-forecast.ts
+++ src/js/modules/toggle-forecast.ts
@@ -2,27 +2,25 @@ import { ElementsSelector } from 'types/types';
 
 export default function (): void {
   const ELEMENTS: ElementsSelector = {
-    forecastParent: document.querySelector('[data-forecast]'),
-    forecastHeader: document.querySelector('[data-forecast-header]'),
-    forecastCaption: document.querySelector('[data-forecast-caption]'),
-    forecastBody: document.querySelector('[data-forecast-body]')
+    parent: document.querySelector('[data-forecast]'),
+    toggle: document.querySelector('[data-forecast-header-wrapper]'),
+    caption: document.querySelector('[data-forecast-caption]'),
+    body: document.querySelector('[data-forecast-body]')
   };
 
   const activeClass = 'weather-card__forecast--active';
   const defaultCaption = 'Show forecast';
   const activeCaption = 'Hide forecast';
 
-  ELEMENTS.forecastHeader?.addEventListener('click', () => {
-    ELEMENTS.forecastParent?.classList.toggle(activeClass);
+  ELEMENTS.toggle?.addEventListener('click', () => {
+    ELEMENTS.parent?.classList.toggle(activeClass);
 
-    if (ELEMENTS.forecastBody?.style.maxHeight) {
-      ELEMENTS.forecastBody!.style.maxHeight = '';
-      ELEMENTS.forecastCaption!.textContent = defaultCaption;
+    if (ELEMENTS.body?.style.maxHeight) {
+      ELEMENTS.body!.style.maxHeight = '';
+      ELEMENTS.caption!.textContent = defaultCaption;
     } else {
-      ELEMENTS.forecastBody!.style.maxHeight = `${
-        ELEMENTS.forecastBody!.scrollHeight
-      }px`;
-      ELEMENTS.forecastCaption!.textContent = activeCaption;
+      ELEMENTS.body!.style.maxHeight = `${ELEMENTS.body!.scrollHeight}px`;
+      ELEMENTS.caption!.textContent = activeCaption;
     }
   });
 }
