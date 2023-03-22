07bb38f81e3b095ca07b41511d7cad2c497fd169
Qusz <de.gorchakov@gmail.com>
2023-03-22 14:14:33 +0300

    adds content loading on page load

---
+++ src/js/modules/load-content.ts
@@ -0,0 +1,64 @@
+import { checkLocalStorage, getSavedLocation } from 'utils/local-storage';
+
+import getLocation from '../APIs/geo-js';
+import openmeteo from '../APIs/openmeteo';
+import worldtime from '../APIs/worldtime';
+
+import renderCurrentWeather from './render-current-weather';
+import renderForecast from './render-forecast';
+import renderLocation from './render-location';
+import renderCurrentDatetime from './render-current-datetime';
+
+export default function (): void {
+  document.addEventListener('DOMContentLoaded', () => {
+    const localStorageExists = checkLocalStorage();
+
+    if (localStorageExists) {
+      const location = getSavedLocation();
+
+      renderLocation(location.city, location.country);
+
+      openmeteo(location.lat, location.lon)
+        .then((data) => {
+          renderCurrentWeather(data);
+          return data;
+        })
+        .then((data) => {
+          renderForecast(data);
+          return data;
+        })
+        .then((data) => {
+          return worldtime(data.timezone);
+        })
+        .then((data) => {
+          renderCurrentDatetime(data.datetime, data.timezone);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    } else {
+      getLocation()
+        .then((location) => {
+          renderLocation(location.city, location.country);
+          return openmeteo(location.latitude, location.longitude);
+        })
+        .then((weatherData) => {
+          renderCurrentWeather(weatherData);
+          return weatherData;
+        })
+        .then((weatherData) => {
+          renderForecast(weatherData);
+          return weatherData;
+        })
+        .then((weatherData) => {
+          return worldtime(weatherData.timezone);
+        })
+        .then((timeData) => {
+          renderCurrentDatetime(timeData.datetime, timeData.timezone);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  });
+}
